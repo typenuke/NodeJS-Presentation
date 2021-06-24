4b6a8563c5c228289ecc8e2f2a6c32f3563c56f4
shopwn <bismainlearn@gmail.com>
2021-06-24 23:13:06 +0700

    Edit something !!

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,3 +1,6 @@
+//  NODEMAILER
+//  NODEMON
+
 const express = require('express');
 const app = express();
 
@@ -12,17 +15,30 @@ app.post('/', (req, res)=>{
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'username@gmail.com',
-            pass: 'password'
+            user: 'nodejsmailertest00@gmail.com',
+            pass: 'P@ssw0rd!!!'
         }
     })
 
     const mailOptions = {
-        from: req.body.email,
+        from: "Nodemailer Contact <nodejsmailertest00@gmail.com>",
         to: 'bismainlearn@gmail.com',
-        subject: `Message from ${req.body.email}: ${req.body.subject}`,
-        text: req.body.comments
+        subject: `Nodemailer Contact !!!`,
+        html: output
     }
+
+    const output = `
+        <h2 style="color:red;">You have new contact request</h2>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Subject: ${req.body.subject}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.comments}</p>
+    `
+    
     transporter.sendMail(mailOptions, (error, info)=>{
         if(error){
             console.log(error);
